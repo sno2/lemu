cd9065ce801744e549fccca6d29e1eee238d2fec
Carter Snook <cartersnook04@gmail.com>
2025-12-09 11:09:53 -0600

    consent to downloads in extension and better cache checks

--- editors/vscode/src/extension.ts
+++ editors/vscode/src/extension.ts
@@ -20,41 +20,94 @@ import path from "path";
 const name = "lemu";
 const displayName = "Lemu Language Server";
 const args = ["--stdio"];
-const expireMs = 30 * 24 * 60 * 1000; // 30 days
+const checkMs = 3 * 24 * 60 * 1000; // 3 days
 const remote = "https://codeberg.org/sno2/lemu/releases/download/latest/";
 
+const CHECK_INSTALL_TIME_KEY = `lemu.checkInstallTime`;
+const INSTALL_LAST_MODIFIED_KEY = `lemu.installLastModified`;
+const INSTALL_ALWAYS_KEY = `lemu.installAlways`;
+
 async function fetchBinary(
+  context: ExtensionContext,
   channel: OutputChannel,
   binaryUri: Uri,
-  binaryName: string,
-  wantBytes: boolean
+  binaryName: string
 ) {
+  const checkInstallTime = context.globalState.get(CHECK_INSTALL_TIME_KEY, 0);
+  const shouldCheckInstall = Date.now() - checkInstallTime >= checkMs;
+  let installLastModified = new Date(
+    context.globalState.get(INSTALL_LAST_MODIFIED_KEY, 0)
+  );
+
+  let binary: Uint8Array | undefined;
   try {
-    const stat = await workspace.fs.stat(binaryUri);
-    if (new Date().getTime() - stat.ctime < expireMs) {
-      channel.appendLine("(JS) Cache hit for binary.");
-      return wantBytes ? workspace.fs.readFile(binaryUri) : undefined;
+    binary = await workspace.fs.readFile(binaryUri);
+    if (!shouldCheckInstall) {
+      channel.appendLine("(JS) Using existing binary");
+      return binary;
     }
-    throw "too old"; // yeet
   } catch (err) {
-    channel.appendLine(`(JS) Cache miss for binary: ${err}`);
+    channel.appendLine(`(JS) Failed to access binary: ${err}`);
+    installLastModified = new Date(0);
   }
 
-  return window.withProgress(
-    {
-      location: ProgressLocation.Notification,
-      title: `Downloading ${binaryName}`,
-    },
-    async () => {
-      const input = `${remote}${binaryName}`;
-      channel.appendLine(`(JS) Fetching binary: ${input}`);
-      const response = await fetch(input);
-      const bytes = await response.bytes();
-      channel.appendLine(`(JS) Writing binary: "${binaryUri.toString()}"`);
-      await workspace.fs.writeFile(binaryUri, bytes as any);
-      return bytes;
+  try {
+    const url = `${remote}${binaryName}`;
+    const response = await fetch(url);
+
+    const lastModified = new Date(response.headers.get("Last-Modified") ?? 1);
+    if (installLastModified.getTime() === lastModified.getTime()) {
+      channel.appendLine(
+        '(JS) Using existing binary that matches "Last-Modified" of latest'
+      );
+      return binary;
     }
-  );
+
+    if (!context.globalState.get(INSTALL_ALWAYS_KEY, false)) {
+      const result = await window.showInformationMessage(
+        binary
+          ? "Lemu executable is out of date. Would you like to update it?"
+          : "Lemu executable not found. Would you like to download it?",
+        "Yes (Always)",
+        "Yes",
+        "Cancel"
+      );
+
+      if (result === "Yes (Always)") {
+        await context.globalState.update(INSTALL_ALWAYS_KEY, true);
+      } else if (result === "Cancel") {
+        throw new Error("user cancellation");
+      }
+    }
+
+    return await window.withProgress(
+      {
+        location: ProgressLocation.Notification,
+        title: `Downloading ${binaryName}`,
+      },
+      async () => {
+        channel.appendLine(`(JS) Fetching binary: ${url}`);
+        const bytes = await response.bytes();
+        channel.appendLine(`(JS) Writing binary: "${binaryUri.toString()}"`);
+        await workspace.fs.writeFile(binaryUri, bytes as any);
+        if (!binaryName.endsWith(".wasm") || !binaryName.endsWith(".exe")) {
+          channel.appendLine("(JS) Making binary executable.");
+          await chmod(binaryUri.fsPath, 0o755);
+        }
+        await Promise.all([
+          context.globalState.update(CHECK_INSTALL_TIME_KEY, Date.now()),
+          context.globalState.update(INSTALL_LAST_MODIFIED_KEY, lastModified),
+        ]);
+        return bytes;
+      }
+    );
+  } catch (err) {
+    channel.appendLine(`(JS) Failed to fetch binary: ${err}`);
+    if (binary) {
+      return binary;
+    }
+    throw err;
+  }
 }
 
 async function getLanguageClient(
@@ -142,30 +195,32 @@ export async function activate(context: ExtensionContext) {
 
   const channel = window.createOutputChannel("Lemu Language Server");
 
-  if (exePath) {
-    channel.appendLine(`(JS) Using existing executable path: "${exePath}"`);
-    client = await getLanguageClient(channel, Uri.file(exePath), undefined);
-  } else {
-    const binaryName = isBrowser ? "lemu-wasm32-wasi.wasm" : getBinaryName();
-    const ext =
-      binaryName.indexOf(".") !== -1
-        ? binaryName.slice(binaryName.indexOf("."))
-        : "";
+  try {
+    if (exePath) {
+      channel.appendLine(`(JS) Using existing executable path: "${exePath}"`);
+      client = await getLanguageClient(channel, Uri.file(exePath), undefined);
+    } else {
+      const binaryName = isBrowser ? "lemu-wasm32-wasi.wasm" : getBinaryName();
+      const ext =
+        binaryName.indexOf(".") !== -1
+          ? binaryName.slice(binaryName.indexOf("."))
+          : "";
 
-    const binaryUri = Uri.joinPath(context.globalStorageUri, `lemu${ext}`);
-    const binary = await fetchBinary(channel, binaryUri, binaryName, isBrowser);
-    if (!isBrowser && !binaryName.endsWith(".exe")) {
-      channel.appendLine("(JS) Adding executable flags to binary");
-      await chmod(binaryUri.fsPath, 0o755);
-    }
-    if (!isBrowser) {
-      context.environmentVariableCollection.prepend(
-        "PATH",
-        `${context.globalStorageUri.fsPath}${path.delimiter}`
-      );
-    }
+      const binaryUri = Uri.joinPath(context.globalStorageUri, `lemu${ext}`);
+      const binary = await fetchBinary(context, channel, binaryUri, binaryName);
 
-    client = await getLanguageClient(channel, binaryUri, binary);
+      if (!isBrowser) {
+        context.environmentVariableCollection.prepend(
+          "PATH",
+          `${context.globalStorageUri.fsPath}${path.delimiter}`
+        );
+      }
+
+      client = await getLanguageClient(channel, binaryUri, binary);
+    }
+  } catch (e) {
+    window.showErrorMessage(`Failed to get Lemu executable: ${e}`);
+    return;
   }
 
   await client.start();
